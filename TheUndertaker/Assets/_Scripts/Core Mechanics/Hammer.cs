43945a38019b1a94002620683be5b73df1c336da
Liana Tomecek <liana.tomecek@gmail.com>
2025-10-13 13:25:07 -0600

    nails can be hit

--- TheUndertaker/Assets/_Scripts/Core Mechanics/Hammer.cs
+++ TheUndertaker/Assets/_Scripts/Core Mechanics/Hammer.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 
 [RequireComponent (typeof(Collider2D))]
@@ -13,6 +14,8 @@ public class Hammer : MonoBehaviour
     void Start()
     {
         InputHandler.Instance.NewMovement.AddListener(SetDirectionalInput);
+        InputHandler.Instance.ButtonPressed.AddListener(TryHit);
+
         _boundsMin = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
         _boundsMax = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
     }
@@ -21,19 +24,11 @@ public class Hammer : MonoBehaviour
     {
         UpdatePosition();
     }
-
-
-    #region Hitting Nails
-    void OnTriggerEnter2d(Collider2D other)
-    {
-        IHittable hittable = other.gameObject.GetComponent<IHittable>();
-        if (hittable != null)
-        {
-            //highlight whackable nail here
-            _hoveredHittable = hittable;
-        }
-    }
-
+    
+    //-----------------
+    // INPUT HANDLING
+    //-----------------
+    #region Player Input and Movement
     public void TryHit()
     {
         //play "hitting" animation, even if there is nothing to hit
@@ -41,12 +36,8 @@ public class Hammer : MonoBehaviour
         //play whoose/swing sound effect
 
         _hoveredHittable?.Hit();
+        UnityEngine.Debug.Log("Tried to hit");
     }
-    #endregion
-
-
-
-    #region Player Input and Movement
     private void SetDirectionalInput(Vector2 input)
     {
         _directionalInput = input;
@@ -55,11 +46,23 @@ public class Hammer : MonoBehaviour
     private void UpdatePosition()
     {
         _newPosition = transform.position + ((Vector3)_directionalInput * _movementSpeed * Time.deltaTime);      //effectively a transform.Translate
-        
+
         _newPosition.x = Mathf.Clamp(_newPosition.x, _boundsMin.x, _boundsMax.x);                       //Keeps the hammer within the bounds of the screen
         _newPosition.y = Mathf.Clamp(_newPosition.y, _boundsMin.y, _boundsMax.y);
-        
+
         transform.position = _newPosition;
+    }
+    #endregion
+    
+
+
+    //---------------------
+    //  GETTERS & SETTERS
+    //---------------------
+    #region Getters/Setters
+    public void SetHittable(IHittable hittable)
+    {
+        _hoveredHittable = hittable;
     }
     #endregion
 
